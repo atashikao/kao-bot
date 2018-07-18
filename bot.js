1c559de46da3394e3d62ab5ce81cf9b0e455ddd2
atashikao <30452212+atashikao@users.noreply.github.com>
2018-07-19 02:14:41 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,20 +12,3 @@ client.on('message', message => {
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
-
-client.login('NDU3NTExNTI4MTU0MDA1NTI0.DjEguA.XLD7jRPgVkc3Q72Dv90aiKDjitk');
