4d050eca101e6d116994356260548b47671786d8
atashikao <30452212+atashikao@users.noreply.github.com>
2018-07-19 02:45:25 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    //status
+    bot.user.setGame('kao',https://www.twitch.tv/atashikao'); //
 });
 
 client.on('message', message => {
